5386a3db32539788f1b8bf29d4044f1aae8e3c6d
qingsheng <qingsheng86@sina.com>
2018-06-11 11:14:16 +0800

    加入和创建数据库和表的各项配置

--- app/src/main/java/com/qingyaweather/android/MainActivity.java
+++ app/src/main/java/com/qingyaweather/android/MainActivity.java
@@ -1,13 +1,24 @@
 package com.qingyaweather.android;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
